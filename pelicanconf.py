fefb1ffbea3ee6513066f4c994afaf93286c3505
Joakim Bech <joakim.bech@linaro.org>
2018-08-18 23:16:55 +0000

    Update blueidea to not show full article
    Signed-off-by: Joakim Bech <joakim.bech@linaro.org>

--- pelicanconf.py
+++ pelicanconf.py
@@ -29,15 +29,40 @@ LINKS = (('Pelican', 'http://getpelican.com'),
          ('Linaro', 'https://linaro.org'),)
 
 # Social widget
-SOCIAL = (('Jyxxan @ Facebook', 'https://www.facebook.com/Jyxxan'),
-          ('Jyxxan @ Twitter', 'https://twitter.com/Jyxxan'),
-          ('Joakim @ LinkedIn', 'https://www.linkedin.com/in/jbech'),
-          ('Joakim @ GitHub', 'https://github.com/jbech-linaro'),)
+#SOCIAL = (('Jyxxan @ Facebook', 'https://www.facebook.com/Jyxxan'),
+#          ('Jyxxan @ Twitter', 'https://twitter.com/Jyxxan'),
+#          ('Joakim @ LinkedIn', 'https://www.linkedin.com/in/jbech'),
+#          ('Joakim @ GitHub', 'https://github.com/jbech-linaro'),)
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 THEME = 'pelican-themes/blueidea'
+DISPLAY_CATEGORIES_ON_MENU = False
+DISPLAY_CATEGORIES_ON_SUBMENU = True
+DISPLAY_AUTHOR_ON_POSTINFO = True
+DISPLAY_CATEGORIES_ON_POSTINFO = True
+DISPLAY_PAGES_ON_MENU = False
+DISPLAY_SEARCH_FORM = False
+
+#THEME = 'pelican-themes/Just-Read/'
+#THEME = 'pelican-themes/nice-blog/'
+#THEME = 'pelican-themes/Nuja/'
+
+#THEME = 'pelican-themes/pelican-blue/'
+SOCIAL = (('linkedin', 'https://www.linkedin.com/in/jbech'),
+          ('github', 'https://github.com/jbech-linaro'),
+          ('twitter', 'https://twitter.com/Jyxxan'),
+          ('facebook', 'https://www.facebook.com/Jyxxan'),
+          )
+
+#THEME = 'pelican-themes/storm'
+#THEME = 'pelican-themes/sundown'
 
 #LOAD_CONTENT_CACHE = False
+DISPLAY_PAGES_ON_MENU = True
+SUMMARY_MAX_LENGTH = 80
+
+PLUGIN_PATHS = ['pelican-plugins']
+PLUGINS = ['assets']
